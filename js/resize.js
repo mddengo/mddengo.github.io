0a34d48aefc8084a88b2b97ef994b99ebb35d00d
mddengo <mddengo@gmail.com>
2017-05-09 22:43:11 -0400

    fiddled with flowjs

--- js/resize.js
+++ js/resize.js
@@ -1,9 +1,9 @@
 $('body').flowtype({
     // minimum   : 500,
     // maximum   : 1200,
-    // minFont   : 12,
-    // maxFont   : 40,
-    // fontRatio : 30
+    minFont   : 12,
+    maxFont   : 40,
+    fontRatio : 30
 });
     // $('article').flowtype({
     //     fontRatio : 36
