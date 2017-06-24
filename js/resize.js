cad808c38c0be48e2ea2fc1970271026840bc5b6
mddengo <mddengo@gmail.com>
2017-06-23 20:48:56 -0700

    index resize

--- js/resize.js
+++ js/resize.js
@@ -1,7 +1,12 @@
 $('body').flowtype({
-    minimum : 200,
-    maximum : 1200,
-    minFont   : 12,
-    maxFont   : 40,
-    fontRatio : 30
+    // minimum : 500,
+    // maximum : 1200,
+    // minFont   : 12,
+    // maxFont   : 40,
+    // fontRatio : 20
+}),
+$('container').flowtype({
+    minFont: 24,
+    maxFont: 36,
+    fontRatio: 20
 });
