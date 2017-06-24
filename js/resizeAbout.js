74079a92e5424247ed9945b6c6f064d84cc3216e
mddengo <mddengo@gmail.com>
2017-06-23 21:49:29 -0700

    about header flowtype

--- js/resizeAbout.js
+++ js/resizeAbout.js
@@ -1,15 +1,19 @@
 $('body').flowtype({
 }),
-$('h1').flowtype({
-    minFont: 20,
-    maxFont: 36,
-    fontRatio: 10
+$('nav-container').flowtype({
+    minFont: 24,
+    maxFont: 36
 }),
-$('nav-links').flowtype({
-    minFont: 7,
-    maxFont: 18,
-    // fontRatio: 5
-}),
+// $('h1').flowtype({
+//     minFont: 20,
+//     maxFont: 36,
+//     fontRatio: 10
+// }),
+// $('nav-links').flowtype({
+//     minFont: 7,
+//     maxFont: 18,
+//     // fontRatio: 5
+// }),
 $('about-text').flowtype({
     minFont: 10,
     maxFont: 14
